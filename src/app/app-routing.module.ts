d150e9117f1511d54a100a32dd82e3b1158fa59a
Sagar V <sagarv@leotechnosoft.net>
2021-11-15 14:44:28 +0530

    module5demo1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,51 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component'
+import { ServersComponent } from './servers/servers.component'
+import { ServerComponent } from './servers/server/server.component'
+import { EditServerComponent } from './servers/edit-server/edit-server.component'
+import { UsersComponent } from './users/users.component'
+import { UserComponent } from './users/user/user.component'
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'servers',
+    component: ServersComponent,
+  children: [
+  {
+    path: ':id',
+    component: ServerComponent
+  },
+  {
+    path: ':id/edit',
+    component: EditServerComponent
+  }
+  ]
+  },
+  {
+    path: 'users',
+    component: UsersComponent,
+    children: [
+  {
+    path: ':id/:name',
+    component: UserComponent
+  }
+  ]
+  },
+  {
+  path: '**',
+  redirectTo: '/not-found',
+  pathMatch: 'full'
+  }
+  ];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ServersComponent } from './servers/servers.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { ServerComponent } from './servers/server/server.component';
+import { UsersComponent } from './users/users.component';
+import { UserComponent } from './users/user/user.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ServersComponent,
+    EditServerComponent,
+    ServerComponent,
+    UsersComponent,
+    UserComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
